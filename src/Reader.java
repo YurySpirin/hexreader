7358dd55c08e38dd2ad43047f7984b95d49ac312
YurySpirin <45099821+YurySpirin@users.noreply.github.com>
2018-12-18 10:33:20 +0300

    1.2

--- src/Reader.java
+++ src/Reader.java
@@ -11,20 +11,25 @@ public class Reader {
 
         try {
             FileReader reader = new FileReader("C:\\Drivers\\MCB07l_Test_BlinckLED_Boot.hex");
-            BufferedReader read = new BufferedReader(reader);
-            // считаем сначала первую строку
-            /*String line = read.readLine();*/
-            String line = null;
-            while ((line = read.readLine()) != null) {
+            BufferedReader  read = new BufferedReader(reader);
+            String s = ":10385000814F0008814F0008814F0008814F000808";
+            String line = read.readLine();
+            /*System.out.println(line);*/
+            while (line != null) {
                 System.out.println(line);
                 line = read.readLine();
-                // считываем остальные строки в цикле
-
             }
-            byte[] str = new byte[line.length()];
-            for (int i = 0; i < line.length(); i++)
-                str[i] = (byte) Integer.parseInt((line), 16);
-            System.out.printf("%X ", str);
+           /* while (line != null);
+            {
+                System.out.println(line);
+                line = read.readLine();*/
+
+            byte[] data = new byte[s.length() / 2];
+            for (int i = 0; i < s.length(); i++) {
+                data[i] = (byte) (Integer.parseInt(s,16));
+                System.out.printf(HexBin.encode(data));
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
