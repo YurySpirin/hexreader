cd0ecc6d523e6464433ac7aeafa773ef69a47a7b
YurySpirin <45099821+YurySpirin@users.noreply.github.com>
2019-01-09 15:19:24 +0300

    2.1
    работает парсер

--- src/Reader.java
+++ src/Reader.java
@@ -1,12 +1,10 @@
 import com.sun.istack.internal.NotNull;
 import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
-
 import java.io.*;
 import java.sql.Array;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import static java.lang.String.valueOf;
 
 public class Reader {
@@ -18,47 +16,27 @@ public class Reader {
             FileReader reader = new FileReader("C:\\Drivers\\MCB07l_Test_BlinckLED_Boot.hex");
             BufferedReader read = new BufferedReader(reader);
             String s = ":10385000814F0008814F0008814F0008814F000808";
-            String line = ":10385000814F0008814F0008814F0008814F000808";
-            /*System.out.println(line);*/
-           /* while (line != null) {
-                System.out.println(line);
-                line = read.readLine();*/
+            String line = new String();
+            while (line != null) {
+                line = read.readLine();
                 Pattern p = Pattern.compile(":(.*)");
                 Matcher m = p.matcher(line);
                 while (m.find()) {
                     line = m.group(1);
                 }
-
-                /*System.out.println(line);*/
-
-
-
-           /* while (line != null);
-            {
-                System.out.println(line);
-                line = read.readLine();*/
-                String [] SS = new String [line.length()/2];
+                String[] SS = new String[line.length() / 2];
                 int k = 0;  //счетчик для индексов  массива
-                for(int i=0;i<line.length()-1;i=i+2) { // проход по массиву через каждые 2 символа для нахождения новой подстроки
-                    String S1 = line.substring(i, i+2); // нахождение подстроки с длиной в 2 символа
-                    SS[k++]=S1; // присваивание  подстроки к элементу массива
+                for (int i = 0; i < line.length() - 1; i = i + 2) { // проход по массиву через каждые 2 символа для нахождения новой подстроки
+                    String S1 = line.substring(i, i + 2); // нахождение подстроки с длиной в 2 символа
+                    SS[k++] = S1; // присваивание  подстроки к элементу массива
                 }
-                System.out.println(Arrays.toString(SS));
                 byte[] data = new byte[SS.length];
                 for (int i = 0; i < SS.length; i++) {
 
-                    data[i] = (byte) Integer.parseInt(SS[i],16);
-
-                    System.out.printf("%02X ",(data[i]));
+                    data[i] = (byte) Integer.parseInt(SS[i], 16);
                 }
-               /* String[] lines = line.split("(?<=\\\\G.{2})");
-                System.out.println(Arrays.toString(lines));
-                String qwe = lines[1];
-                System.out.println(qwe);*/
-
-
-                /*System.out.printf("%X ", data);*/
-
+                System.out.println(HexBin.encode(data));
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -69,6 +47,5 @@ public class Reader {
         } catch (ArrayIndexOutOfBoundsException ex) {
             System.out.println(ex);
         }
-
     }
 }
